b8f9618f620b30c626da142da5cb99a0d9c84f59
MasonSimpson <158009210+MasonSimpson@users.noreply.github.com>
2026-01-30 22:59:04 -0600

    Added music. Song is Vindstilla by Silver Maple with a rain track looped behind it.

--- core/src/main/java/com/stormidle/Storm.java
+++ core/src/main/java/com/stormidle/Storm.java
@@ -1,11 +1,19 @@
 package com.stormidle;
 
 import com.stormidle.screens.IntroScreen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.Gdx;
 
 public class Storm extends com.badlogic.gdx.Game {
 
+    Music music;
+
     @Override
     public void create() {
+        // Start playing music
+        music = Gdx.audio.newMusic(Gdx.files.internal("game_music.wav"));
+        music.play();
+        music.setLooping(true);
         // Game holds the active Screen. setScreen swaps it
         setScreen(new IntroScreen(this));
     }
